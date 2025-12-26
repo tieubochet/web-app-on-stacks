351ba3b2681b895ade8f41650999ae5d21277f9d
tieubochet <tao.nguyen712@gmail.com>
2025-12-26 15:54:53 +0700

    Update: Config for Stacks network

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,8 +1,9 @@
 // src/config/index.tsx
 import { BitcoinAdapter } from '@reown/appkit-adapter-bitcoin'
-// Đảm bảo KHÔNG import { bitcoin } từ thư viện networks
+// TUYỆT ĐỐI KHÔNG import { bitcoin } từ networks
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
+// Định nghĩa mạng Stacks
 export const stacks: AppKitNetwork = {
   id: 'stacks:1',
   chainId: '1',
@@ -17,7 +18,7 @@ export const stacks: AppKitNetwork = {
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || 'your-id'
 
-// CHỈ CÓ STACKS
+// Danh sách networks chỉ được chứa [stacks]
 export const networks = [stacks]
 
 export const bitcoinAdapter = new BitcoinAdapter({
